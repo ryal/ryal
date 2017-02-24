7c5760d6436629a69e4730a2e6e11484e3cb180a
Ben A. Morgan <ben@benmorgan.io>
2017-02-24 15:46:17 -0500

    move the mix db config to the global config

--- apps/ryal_core/config/config.exs
+++ apps/ryal_core/config/config.exs
@@ -2,4 +2,8 @@ use Mix.Config
 
 config :ryal_core, ecto_repos: [Ryal.Repo]
 
-import_config "#{Mix.env}.exs"
+config :ryal_core, Ryal.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  database: "ryal_#{Mix.env}",
+  username: System.get_env("RYAL_DB_USER") || System.get_env("USER")
