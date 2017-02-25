c7cad2d98cdac2c712566c100930e308db9ad903
Ben A. Morgan <ben@benmorgan.io>
2017-02-25 18:55:10 -0500

    add a migration for payments

---
+++ apps/ryal_core/priv/repo/migrations/20170225213105_create_ryal_payments.exs
@@ -0,0 +1,15 @@
+defmodule Ryal.Repo.Migrations.CreateRyalPayments do
+  use Ecto.Migration
+
+  def change do
+    create table(:ryal_payments) do
+      add :number, :string, null: false
+      add :state, :string, default: "pending", null: false
+      add :amount, :decimal, null: false
+
+      timestamps()
+    end
+
+    create unique_index :ryal_payments, :amount
+  end
+end
