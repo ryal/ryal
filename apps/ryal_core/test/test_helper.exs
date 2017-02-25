7c5ec6889e576d68eedf3da97cb6b88be652ea82
Ben A. Morgan <ben@benmorgan.io>
2017-02-25 17:37:42 -0500

    cleanup the test helper with a model case

--- apps/ryal_core/test/test_helper.exs
+++ apps/ryal_core/test/test_helper.exs
@@ -1,19 +1,4 @@
-defmodule Ryal.Core.TestCase do
-  use ExUnit.CaseTemplate
-
-  using(opts) do
-    quote do
-      use ExUnit.Case, unquote(opts)
-      import Ecto.Query
-    end
-  end
-
-  setup do
-    Ecto.Adapters.SQL.Sandbox.mode(Ryal.Core.Repo, :manual)
-
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Ryal.Core.Repo)
-  end
-end
+Code.load_file("test/support/model_case.exs")
 
 Ryal.Repo.start_link
 ExUnit.start()
