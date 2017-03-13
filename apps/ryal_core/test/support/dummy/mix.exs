f1dfbfd8b7f90c4f63cc540f7512b0816fb9a010
Ben A. Morgan <ben@benmorgan.io>
2017-03-13 17:51:59 -0400

    add deps to dummy application

--- apps/ryal_core/test/support/dummy/mix.exs
+++ apps/ryal_core/test/support/dummy/mix.exs
@@ -5,7 +5,11 @@ defmodule Dummy.Mixfile do
     [
       app: :dummy,
       config_path: "config/config.exs",
-      version: "1.0.0"
+      version: "1.0.0",
+      deps: [
+        {:ecto, "~> 2.1"},
+        {:postgrex, ">= 0.13.0"}
+      ]
     ]
   end
 end
