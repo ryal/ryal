fed19e5f9b1ea7c0e4686e57b38c7923ef31aca3
Ben A. Morgan <ben@benmorgan.io>
2017-03-14 11:12:48 -0400

    refactor the project_migration_dir to a constant

--- apps/ryal_core/lib/mix/tasks/ryal.core.install.ex
+++ apps/ryal_core/lib/mix/tasks/ryal.core.install.ex
@@ -3,8 +3,10 @@ defmodule Mix.Tasks.Ryal.Core.Install do
 
   use Mix.Task
 
+  @project_migration_dir Mix.Ecto.migrations_path(Ryal.repo())
+
   def run(_) do
-    File.mkdir_p project_migration_dir()
+    File.mkdir_p @project_migration_dir
 
     stamp = String.to_integer timestamp()
 
@@ -12,7 +14,7 @@ defmodule Mix.Tasks.Ryal.Core.Install do
     |> Stream.with_index
     |> Enum.each(fn({ryal_core_migration, index}) ->
       migration = "#{stamp + index}_#{migration_name(ryal_core_migration)}"
-      copy_migration(ryal_core_migration, project_migration_dir(), migration)
+      copy_migration(ryal_core_migration, migration)
     end)
   end
 
@@ -25,11 +27,11 @@ defmodule Mix.Tasks.Ryal.Core.Install do
     |> Enum.reject(&(&1 =~ ~r/ryal_core\.exs$/))
   end
 
-  defp copy_migration(from, to, file_name) do
-    if migration_exists?(to, file_name) do
+  defp copy_migration(from, file_name) do
+    if migration_exists?(file_name) do
       IO.puts "#{file_name} already exists."
     else
-      File.cp_r!(from, "#{to}/#{file_name}")
+      File.cp_r!(from, "#{@project_migration_dir}/#{file_name}")
       IO.puts "Copied #{file_name}"
     end
   end
@@ -42,16 +44,12 @@ defmodule Mix.Tasks.Ryal.Core.Install do
   defp pad(i) when i < 10, do: << ?0, ?0 + i >>
   defp pad(i), do: to_string(i)
 
-  defp migration_exists?(to, file_name) do
-    to
+  defp migration_exists?(file_name) do
+    @project_migration_dir
     <> "/*.exs"
     |> Path.wildcard
     |> Enum.map(&get_migration_name/1)
     |> Enum.member?(get_migration_name file_name)
-  end
-
-  defp project_migration_dir do
-    Mix.Ecto.migrations_path Ryal.repo()
   end
 
   defp migration_name(migration) do
@@ -61,7 +59,7 @@ defmodule Mix.Tasks.Ryal.Core.Install do
   end
 
   defp get_migration_name(migration) do
-    [<<_d::bytes-size(15)>> <> name | _] = migration
+    [<<_::bytes-size(15)>> <> name | _] = migration
       |> String.split("/")
       |> Enum.reverse
 
