c28c96a61d28bbfbb85432d2a7517f405a787d11
Ben A. Morgan <ben@benmorgan.io>
2017-03-14 10:47:28 -0400

    no need for additional configs in test

--- apps/ryal_core/config/test.exs
+++ apps/ryal_core/config/test.exs
@@ -1,11 +1,3 @@
 use Mix.Config
 
-config :logger, level: :warn
-
-config :mime, :types, %{
-  "application/vnd.api+json" => ["json-api"]
-}
-
-config :phoenix, :format_encoders, "json-api": Poison
-
 import_config "../test/support/dummy/config/config.exs"
