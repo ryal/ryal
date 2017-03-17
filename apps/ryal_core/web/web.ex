6cf83e8cbde986e4b1c30ee29778c221d28ae486
Ben A. Morgan <ben@benmorgan.io>
2017-03-17 02:06:48 -0400

    remove Phoenix.HTML

--- apps/ryal_core/web/web.ex
+++ apps/ryal_core/web/web.ex
@@ -34,9 +34,6 @@ defmodule Ryal.Web do
       import Phoenix.Controller, only: [
         get_csrf_token: 0, get_flash: 2, view_module: 1
       ]
-
-      # Use all HTML functionality (forms, tags, etc)
-      use Phoenix.HTML
     end
   end
 
