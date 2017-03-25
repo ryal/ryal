19d509b8ef8a6afcbeef4189203ba449e131134e
Ben A. Morgan <ben@benmorgan.io>
2017-03-25 15:54:16 -0400

    add a web file for dummy

---
+++ apps/ryal_core/test/support/dummy/web/web.ex
@@ -0,0 +1,27 @@
+defmodule Dummy.Web do
+  def model do
+    quote do
+      use Ecto.Schema
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+    end
+  end
+
+  def view do
+    quote do
+      use Phoenix.View, root: "web/templates"
+    end
+  end
+
+  def router do
+    quote do
+      use Phoenix.Router
+    end
+  end
+
+  defmacro __using__(which) when is_atom(which) do
+    apply(__MODULE__, which, [])
+  end
+end
