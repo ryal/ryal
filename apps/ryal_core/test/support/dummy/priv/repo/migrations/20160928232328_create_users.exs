4c7baec46869fbd243154e6c8c57bbbd2887c8c3
Ben A. Morgan <ben@benmorgan.io>
2017-03-25 17:43:42 -0400

    remove citest extension from dummy users:

--- apps/ryal_core/test/support/dummy/priv/repo/migrations/20160928232328_create_users.exs
+++ apps/ryal_core/test/support/dummy/priv/repo/migrations/20160928232328_create_users.exs
@@ -2,10 +2,8 @@ defmodule Dummy.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    execute "CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public"
-
     create table(:users) do
-      add :email, :citext, null: false
+      add :email, :string, null: false
 
       timestamps()
     end
