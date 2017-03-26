0500b074261ce6601cdbf86b1049ebf18cb9689b
Ben A. Morgan <ben@benmorgan.io>
2017-03-26 15:34:54 -0400

    namespace db with ryal

--- apps/ryal_core/test/support/dummy/config/config.exs
+++ apps/ryal_core/test/support/dummy/config/config.exs
@@ -17,7 +17,7 @@ config :dummy, Dummy.Repo,
   priv: "../ryal_core/priv/repo",
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
-  database: "dummy_#{Mix.env}",
+  database: "ryal_dummy_#{Mix.env}",
   username: System.get_env("DUMMY_DB_USER") || System.get_env("USER")
 
 config :ryal_core,
