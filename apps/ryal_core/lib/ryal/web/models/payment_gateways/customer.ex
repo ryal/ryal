bc261435d42ae56ab117c97d421eb805fd8c502d
Ben A. Morgan <ben@benmorgan.io>
2017-03-28 17:48:30 -0400

    setup a simple CUD for stripe and bogus customers

--- apps/ryal_core/lib/ryal/web/models/payment_gateways/customer.ex
+++ apps/ryal_core/lib/ryal/web/models/payment_gateways/customer.ex
@@ -3,20 +3,7 @@ defmodule Ryal.PaymentGateway.Customer do
 
   alias Stripe.Customers
 
-  @spec create(atom, Ecto.Schema.t) :: String.t
-
-  @doc """
-  Creates a new customer on Stripe with an email and a simple description.
-  """
-  def create(:stripe, user) do
-    params = %{
-      email: user.email,
-      description: "Customer #{user.id} from Ryal."
-    }
-
-    with {:ok, response} <- Customers.create(params),
-      do: {:ok, response.id}
-  end
+  @spec create(atom, Ecto.Schema.t) :: {:ok, String.t}
 
   @doc "Simple bogus creation for an external_id."
   def create(:bogus, _user) do
@@ -26,5 +13,41 @@ defmodule Ryal.PaymentGateway.Customer do
       |> String.ljust(10, ?0)
 
     {:ok, rand_id}
+  end
+
+  @doc """
+  Creates a new customer on Stripe with an email and a simple description.
+  """
+  def create(:stripe, user) do
+    with {:ok, response} <- Customers.create(stripe_params user),
+      do: {:ok, response.id}
+  end
+
+  @spec update(atom, Ecto.Schema.t) :: {:ok, String.t}
+
+  @doc "Simple bogus updating of user attributes."
+  def update(:bogus, _payment_gateway), do: {:ok, %{}}
+
+  @doc "Updates information on Stripe when the user data changes."
+  def update(:stripe, payment_gateway) do
+    user = payment_gateway.user
+    Customers.update(payment_gateway.external_id, stripe_params(user))
+  end
+
+  @spec delete(atom, Ecto.Schema.t) :: {:ok, String.t}
+
+  @doc "Simple customer deletion on a bogus gateway."
+  def delete(:bogus, _payment_gateway), do: {:ok, %{}}
+
+  @doc "Marks a customer account on Stripe as deleted."
+  def delete(:stripe, payment_gateway) do
+    Customers.delete(payment_gateway.external_id)
+  end
+
+  defp stripe_params(user) do
+    %{
+      email: user.email,
+      description: "Customer #{user.id} from Ryal."
+    }
   end
 end
--- apps/ryal_core/test/models/payment_gateways/customer_test.exs
+++ apps/ryal_core/test/models/payment_gateways/customer_test.exs
@@ -1,13 +1,79 @@
 defmodule Ryal.PaymentGateway.CustomerTest do
   use Ryal.ModelCase
 
+  import Mock
+
   alias Dummy.User
+  alias Ryal.PaymentGateway
   alias Ryal.PaymentGateway.Customer
 
-  describe ".create/1" do
+  setup do
+    customer_data = "test/fixtures/stripe/customer.json"
+      |> File.read!
+      |> Poison.Parser.parse!(keys: :atoms)
+
+    [customer_data: customer_data]
+  end
+
+  describe ".create/2" do
     test "bogus will return an id" do
       {:ok, id} = Customer.create(:bogus, %User{})
       assert String.length(id) == 10
+    end
+
+    test "stripe will return an id", %{customer_data: customer_data} do
+      user = %User{}
+        |> User.changeset(%{email: "ryal@example.com"})
+        |> Repo.insert!
+
+      create_response = fn(_params) -> {:ok, customer_data} end
+      with_mock Stripe.Customers, [create: create_response] do
+        assert {:ok, "cus_AMUcqwTDYlbBSp"} == Customer.create(:stripe, user)
+      end
+    end
+  end
+
+  describe ".update/2" do
+    test "bogus can update" do
+      assert {:ok, %{}} == Customer.update(:bogus, %{})
+    end
+
+    test "stripe will update their customer", %{customer_data: customer_data} do
+      user = %User{}
+        |> User.changeset(%{email: "ryal@example.com"})
+        |> Repo.insert!
+
+      gateway = %PaymentGateway{}
+        |> PaymentGateway.changeset(%{type: "stripe", external_id: "cus_123", user_id: user.id})
+        |> Repo.insert!
+        |> Ryal.repo.preload(:user)
+
+      update_response = fn("cus_123", _params) -> {:ok, customer_data} end
+      with_mock Stripe.Customers, [update: update_response] do
+        assert {:ok, _response} = Customer.update(:stripe, gateway)
+      end
+    end
+  end
+
+  describe ".delete/2" do
+    test "bogus can delete" do
+      assert {:ok, %{}} == Customer.delete(:bogus, %{})
+    end
+
+    test "stripe can delete their customer", %{customer_data: customer_data} do
+      user = %User{}
+        |> User.changeset(%{email: "ryal@example.com"})
+        |> Repo.insert!
+
+      gateway = %PaymentGateway{}
+        |> PaymentGateway.changeset(%{type: "stripe", external_id: "cus_123", user_id: user.id})
+        |> Repo.insert!
+        |> Ryal.repo.preload(:user)
+
+      update_response = fn("cus_123") -> {:ok, customer_data} end
+      with_mock Stripe.Customers, [delete: update_response] do
+        assert {:ok, _response} = Customer.delete(:stripe, gateway)
+      end
     end
   end
 end
