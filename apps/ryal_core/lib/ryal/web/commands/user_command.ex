0b355fcdf4b9ba294de4b55c6c66b4ebe691e765
Ben A. Morgan <ben@benmorgan.io>
2017-03-30 20:41:41 -0400

    add user command update and delete

--- apps/ryal_core/lib/ryal/web/commands/user_command.ex
+++ apps/ryal_core/lib/ryal/web/commands/user_command.ex
@@ -10,11 +10,32 @@ defmodule Ryal.UserCommand do
   Creates a new user and iterates over all of the enabled payment gateways to
   setup a profile for them.
   """
-  @spec create(Ecto.Changeset.t) :: {:ok, Ecto.Changeset.t}
+  @spec create(Ecto.Changeset.t) :: {:ok, Ecto.Schema.t}
   def create(changeset) do
-    with {:ok, changeset} <- Ryal.repo.insert(changeset),
+    with {:ok, user} <- Ryal.repo.insert(changeset),
          {:ok, _payment_gateway} <-
-            PaymentGatewayCommand.create(:bogus, changeset),
-      do: {:ok, changeset}
+            PaymentGatewayCommand.create(:bogus, user),
+      do: {:ok, user}
+  end
+
+  @doc """
+  When you change the information of a user, we also tell the payment gateway
+  that it needs to update its information of your user. Doing so is run async.
+  """
+  @spec update(Ecto.Changeset.t) :: {:ok, Ecto.Schema.t}
+  def update(changeset) do
+    with {:ok, user} <- Ryal.repo.update(changeset),
+         [_] <- PaymentGatewayCommand.update(user),
+      do: {:ok, user}
+  end
+
+  @doc """
+  When a user is deleted, we first delete its records from the payment gateways
+  (async) and then finally delete the user from the DB.
+  """
+  @spec delete(Ecto.Schema.t) :: {:ok, Ecto.Schema.t}
+  def delete(user) do
+    with [_] <- PaymentGatewayCommand.delete(user),
+      do: Ryal.repo.delete(user)
   end
 end
--- apps/ryal_core/test/commands/user_command_test.exs
+++ apps/ryal_core/test/commands/user_command_test.exs
@@ -1,9 +1,12 @@
 defmodule Ryal.UserCommandTest do
   use Ryal.ModelCase
 
+  import Mock
+
   alias Dummy.User
 
   alias Ryal.PaymentGateway
+  alias Ryal.PaymentGatewayCommand
   alias Ryal.UserCommand
 
   describe ".create/1" do
@@ -17,6 +20,60 @@ defmodule Ryal.UserCommandTest do
 
       assert Ryal.repo.one!(User)
       assert Ryal.repo.one!(PaymentGateway)
+    end
+  end
+
+  describe ".update/1" do
+    setup do
+      {:ok, user} = %User{}
+        |> User.changeset(%{email: "ryal@example.com"})
+        |> UserCommand.create
+
+      [user: user]
+    end
+
+    test "will update a user", %{user: user} do
+      {:ok, user} = user
+        |> User.changeset(%{email: "ryal@updated.com"})
+        |> UserCommand.update
+
+      assert user.email == "ryal@updated.com"
+    end
+
+    test "will update payment gateway data", %{user: user} do
+      with_mock PaymentGatewayCommand, [update: fn(_) -> [{:ok, ""}] end] do
+        {:ok, user} = user
+          |> User.changeset(%{email: "ryal@example.com"})
+          |> UserCommand.update
+
+        assert called PaymentGatewayCommand.update(user)
+      end
+    end
+  end
+
+  describe ".delete/1" do
+    setup do
+      {:ok, user} = %User{}
+        |> User.changeset(%{email: "ryal@example.com"})
+        |> UserCommand.create
+
+      [user: user]
+    end
+
+    test "will delete the user from a payment gateway", %{user: user} do
+      {:ok, _} = UserCommand.delete(user)
+
+      assert_raise Ecto.NoResultsError, fn ->
+        Ryal.repo.get!(Ryal.user_module, user.id)
+      end
+    end
+
+    test "will delete the user", %{user: user} do
+      with_mock PaymentGatewayCommand, [delete: &([ok: &1])] do
+        {:ok, _} = UserCommand.delete(user)
+
+        assert called PaymentGatewayCommand.delete(user)
+      end
     end
   end
 end
