8bb1f52b128f117d0404acf3e9355bc624998249
Ben A. Morgan <ben@benmorgan.io>
2017-04-18 16:28:40 -0400

    update specs for proxy changes

--- apps/ryal_core/test/models/payment_methods/proxy_test.exs
+++ apps/ryal_core/test/models/payment_methods/proxy_test.exs
@@ -4,16 +4,22 @@ defmodule Ryal.PaymentMethod.ProxyTest do
   alias Ryal.PaymentMethod.Proxy
   alias Ryal.PaymentMethod.CreditCard
 
+  import Ecto.Changeset, only: [cast: 3]
+
   describe ".changeset/2" do
     test "will work with empty maps" do
-      expectation = Ecto.Changeset.cast(%Proxy{}, %{}, [])
+      expectation = cast(%Proxy{}, %{}, [])
       assert Proxy.changeset(%Proxy{}, %{}) == expectation
     end
 
     test "will select changeset based on struct provided" do
       params = Map.from_struct(%CreditCard{})
-      expectation = CreditCard.changeset(%CreditCard{}, params)
-      assert Proxy.changeset(%Proxy{}, %CreditCard{}) == expectation
+      changeset = CreditCard.changeset(%CreditCard{}, params)
+      expectation = cast(%Proxy{}, %{data: params}, [:data])
+      assert Proxy.changeset(%Proxy{}, %CreditCard{}) == Map.merge(expectation, %{
+          errors: changeset.errors,
+          valid?: false
+        })
     end
   end
 end
